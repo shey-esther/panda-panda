fa70d182e4733900854c01f062d884004f7ca5c8
shey-esther <estheralvaro44@gmail.com>
2017-08-11 18:40:43 -0700

    insertando x

--- js/main.js
+++ js/main.js
@@ -5,8 +5,14 @@ var pictures = ["img/a1.jpg","img/a2.jpg", "img/a3.jpg", "img/a4.jpg"];
 for (var i = 0; i < pictures.length; i++) {
 	pictures[i];
 	imagen.setAttribute('class', 'img');
-	var img = document.createElement('img');
-	img.src =pictures[i];
-	img.setAttribute('class','panda')
-	imagen.appendChild(img);
+	var pandita = document.createElement('img');
+	var x = document.createElement('x');
+	//x.setAttribute('id','x_boton');
+	x.innerHTML='X';
+	pandita.src =pictures[i];
+	pandita.setAttribute('class','panda')
+	
+	imagen.appendChild(pandita);
+	imagen.appendChild(x);
+
 }
